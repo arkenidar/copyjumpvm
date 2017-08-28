69ef2483375c8ba0e2e312856c053aa9e9d10ef6
Dario Cangialosi <dario.cangialosi@gmail.com>
2017-08-28 21:38:29 +0200

    build files (gradle-based and bash-based).

---
+++ sbuild.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+# Thanks to http://www.sergiy.ca/how-to-compile-and-launch-java-code-from-command-line/
+
+NAME=copyjumpvm
+COM="$NAME.Main"
+SRCS=src
+BIN=bin
+
+build() {
+	echo "building..."
+	mkdir -p "$BIN"
+	/usr/bin/javac -d "$BIN" -sourcepath "$SRCS" "$SRCS/$NAME/"*.java
+}
+
+clean() {
+	echo "cleaning..."
+	rm -fr "$SRCS/$NAME/*.class" "$BIN"
+}
+
+run() {
+	echo "running..."
+	/usr/bin/java -cp "$BIN" "$COM"
+}
+
+main() {
+	case $1 in
+	build|clean|run) $1;;
+	*) echo "Usage: $0 [build|clean|run]"; exit 1;;
+	esac
+}
+
+main "$@"
